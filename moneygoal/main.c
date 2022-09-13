bac7a41598c635d48f4a033d5dea6567c380390a
omerbsagir <omerbesagir@gmail.com>
2022-09-13 14:43:50 +0300

    Input has taken

--- moneygoal/main.c
+++ moneygoal/main.c
@@ -6,7 +6,17 @@ int main(void)
     double thegoal, hourpay, hours;
     int days, holidays;
 
-    printf("Please enter your goal\n");
+    printf("What is your money goal?\n");
+    scanf("%lf", &thegoal);
+    printf("How much do you earn money in a hour?\n");
+    scanf("%lf", &hourpay);
+    printf("How many hours do you work in a day?\n");
+    scanf("%lf", &hours);
+    printf("How many days do you work in a week?\n");
+    scanf("%d", &days);
+    printf("Do you have any holidays in a year?\nIf there is how many days is it?\n");
+    scanf("%d", &holidays);
+
 
 
 }
